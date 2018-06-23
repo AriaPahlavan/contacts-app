4ba4d73f84d61785583f6f564fc51e3f78155c53
Aria Pahlavan <apahlavan1@utexas.edu>
2018-06-23 00:22:15 -0500

    add tachyons, add empty card component

---
+++ src/Card.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+const Card = (props) => {
+    return (
+        <div className="fl w-third br4 tc bg-light-green">
+            <h1>Hello</h1>
+        </div>
+    );
+};
+
+
+export default Card;
