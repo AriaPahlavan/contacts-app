68c6c93e60f6e4e96ba422b5904f672ed5039156
Aria Pahlavan <apahlavan1@utexas.edu>
2018-06-23 00:44:35 -0500

    use robohash API's to add random cat avatar to each card.

--- src/Card.js
+++ src/Card.js
@@ -3,8 +3,12 @@ import React from 'react';
 
 const Card = (props) => {
     return (
-        <div className="fl w-third br4 tc bg-light-green">
-            <h1>Hello</h1>
+        <div className="tc dib br3 bw2 pa3 ma2 bg-light-green grow shadow-1">
+            <img src="https://robohash.org/test?set=set4" alt="avatar"/>
+            <div>
+                <h2>Jane Doe</h2>
+                <h3>jane.doe@gmail.com </h3>
+            </div>
         </div>
     );
 };
