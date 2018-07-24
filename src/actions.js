65509160b055e3b3429c80778b68e32f10601414
Aria Pahlavan <apahlavan1@utexas.edu>
2018-07-23 21:35:55 -0500

    create a redux action for fetching users

--- src/actions.js
+++ src/actions.js
@@ -8,4 +8,13 @@ import {
 export const setSearchField = (text) => ({
   type: CHANGE_SEARCH_FIELD,
   payload: text
-})
+});
+
+
+export const requestRobots = () => (dispatch) => {
+  dispatch({ type: REQ_ROBOTS_PEND });
+  fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(users => dispatch({ type: REQ_ROBOTS_SUCC, payload: users }))
+    .catch(error => dispatch({ type: REQ_ROBOTS_FAIL, payload: error }));
+}
