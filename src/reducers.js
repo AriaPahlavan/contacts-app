f5d69b2d68f2a29f661f3ed72d3ae15876e57103
Aria Pahlavan <apahlavan1@utexas.edu>
2018-07-23 21:41:52 -0500

    create a redux reducer for fetching users

--- src/reducers.js
+++ src/reducers.js
@@ -1,4 +1,9 @@
-import {CHANGE_SEARCH_FIELD} from './constants.js';
+import {
+  CHANGE_SEARCH_FIELD,
+  REQ_ROBOTS_PEND,
+  REQ_ROBOTS_SUCC,
+  REQ_ROBOTS_FAIL
+} from './constants.js';
 
 const initialState = {
   searchField: ''
@@ -8,6 +13,22 @@ export const searchRobots = (state=initialState, action={}) => {
   switch (action.type) {
     case CHANGE_SEARCH_FIELD:
       return Object.assign({}, state, {searchField: action.payload});
+    default:
+      return state;
+  }
+}
+
+export const requestRobots = (state=initialState, action={}) => {
+  switch (action.type) {
+    case REQ_ROBOTS_PEND:
+      return Object.assign({}, state, {isPending: true});
+    case REQ_ROBOTS_SUCC:
+      return Object.assign({}, state, {users: action.payload,
+                                       isPending: false});
+    case REQ_ROBOTS_FAIL:
+      return Object.assign({}, state, {users: [],
+                                       isPending: false,
+                                       error: action.payload});
     default:
       return state;
   }
