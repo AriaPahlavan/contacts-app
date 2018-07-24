6678ecf06e029e191b8cf48929adae109ae5ae82
Aria Pahlavan <apahlavan1@utexas.edu>
2018-07-23 21:43:25 -0500

    setup thunk middleware for async redux dispatch

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {createStore, applyMiddleware} from 'redux';
 import {Provider} from 'react-redux'
+import thunkMiddleware from 'redux-thunk';
 import {createLogger} from 'redux-logger';
 import 'tachyons'
 
@@ -13,7 +14,8 @@ import registerServiceWorker from './registerServiceWorker';
 
 
 const logger = createLogger();
-const store = createStore(searchRobots, applyMiddleware(logger));
+const store = createStore(searchRobots, 
+                          applyMiddleware(thunkMiddleware, logger));
 
 ReactDOM.render(
   <Provider store={store}>
