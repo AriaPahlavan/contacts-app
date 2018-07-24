91b00d4133bc887e2b3b5d2122186d35a431cd80
Aria Pahlavan <apahlavan1@utexas.edu>
2018-07-23 20:48:44 -0500

    use redux to store state

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,14 +1,26 @@
 import React, {Component} from 'react';
+import {connect} from 'react-redux';
+
 import CardList from '../components/CardList';
 import SreachBox from '../components/SreachBox';
 import Scroll from '../components/Scroll';
+import {setSearchField} from '../actions.js'
+
+
+const mapStateToProps = state => ({
+  searchField: state.searchField
+})
+
+const mapDispatchToProps = dispatch => ({
+  onSearch: event => dispatch(setSearchField(event.target.value))
+})
+
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      contacts: [],
-      searchfield: ''
+      contacts: []
     }
   }
 
@@ -18,20 +30,20 @@ class App extends Component {
       .then(contacts => this.setState({ contacts: contacts }));
   }
 
-  onSearch = (event) => this.setState({searchfield: event.target.value});
-
   render() {
-    const { contacts, searchfield } = this.state;
+    const { contacts } = this.state;
+    const { searchField, onSearch } = this.props;
+
     const filteredContacts = contacts.filter(contact => {
       return contact.name
                     .toLowerCase()
-                    .includes(searchfield.toLowerCase());
+                    .includes(searchField.toLowerCase());
     });
 
     return (
       <div className='tc'>
         <h1 className="avenir f1 light-green">Contacts List</h1>
-        <SreachBox onSearch={this.onSearch}/>
+        <SreachBox onSearch={onSearch}/>
         <Scroll>
           <CardList contacts={filteredContacts}/>
         </Scroll>
@@ -41,4 +53,4 @@ class App extends Component {
 }
 
 
-export default App;
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {createStore} from 'redux';
+import {Provider} from 'react-redux'
 import 'tachyons'
+
 import './index.css';
 import App from './containers/App';
+import {searchRobots} from './reducers.js'
 
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+const store = createStore(searchRobots);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root'));
 registerServiceWorker();
